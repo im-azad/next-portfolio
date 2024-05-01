37f7c29b7eff0dba0c425b930319e7d141db5aac
im-azad <me.azad99@gmail.com>
2024-05-01 18:18:12 +0600

    navbar motion  animation added

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -3,7 +3,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import NaveLink from "./naveLink";
+import NavLink from "./navLink";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 const links = [
     { url: "/", title: "Home" },
@@ -11,83 +13,147 @@ const links = [
     { url: "/portfolio", title: "Portfolio" },
     { url: "/contact", title: "Contact" },
 ];
+
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const pathName = usePathname();
+    const topVariants = {
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: 45,
+            backgroundColor: "rgb(255,255,255)",
+        },
+    };
+    const centerVariants = {
+        closed: {
+            opacity: 1,
+        },
+        opened: {
+            opacity: 0,
+        },
+    };
+
+    const bottomVariants = {
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: -45,
+            backgroundColor: "rgb(255,255,255)",
+        },
+    };
+
+    const listVariants = {
+        closed: {
+            x: "100vw",
+        },
+        opened: {
+            x: 0,
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.2,
+            },
+        },
+    };
+
+    const listItemVariants = {
+        closed: {
+            x: -10,
+            opacity: 0,
+        },
+        opened: {
+            x: 0,
+            opacity: 1,
+        },
+    };
+
     return (
         <div className="h-full flex items-center justify-between text-xl">
-            {/* links */}
+            {/* LINKS */}
             <div className="hidden md:flex gap-4 w-1/3">
                 {links.map((link) => (
-                    <NaveLink key={link.url} link={link} />
+                    <NavLink link={link} key={link.title} />
                 ))}
             </div>
-            {/*logo here */}
-            <div className="md:hidden lg:flex w-1/3 justify-center">
-                <Link href="/">
-                    <span className="text-3xl text-indigo-600 font-bold">
-                        Logo
+            {/* LOGO */}
+            <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+                <Link
+                    href="/"
+                    className="text-sm bg-indigo-600 rounded-md p-1 font-semibold flex items-center justify-center"
+                >
+                    <span className="text-white mr-1">Azad</span>
+                    <span className="w-12 h-8 rounded bg-white text-indigo-600 flex items-center justify-center">
+                        .dev
                     </span>
                 </Link>
             </div>
-            {/* Social Links */}
-            <div className="hidden md:flex gap-4 w-1/3 justify-end">
-                <Link href="#">
-                    <Image
-                        src="/github.png"
-                        alt="logo"
-                        width={24}
-                        height={24}
-                    />
+            {/* SOCIAL */}
+            <div className="hidden md:flex md:justify-end gap-4 w-1/3">
+                <Link href="/">
+                    <Image src="/github.png" alt="" width={24} height={24} />
                 </Link>
-                <Link href="#">
-                    <Image
-                        src="/linkedin.png"
-                        alt="logo"
-                        width={24}
-                        height={24}
-                    />
+                <Link href="/">
+                    <Image src="/dribbble.png" alt="" width={24} height={24} />
                 </Link>
-                <Link href="#">
-                    <Image
-                        src="/facebook.png"
-                        alt="logo"
-                        width={24}
-                        height={24}
-                    />
+                <Link href="/">
+                    <Image src="/instagram.png" alt="" width={24} height={24} />
                 </Link>
-                <Link href="#">
-                    <Image
-                        src="/instagram.png"
-                        alt="logo"
-                        width={24}
-                        height={24}
-                    />
+                <Link href="/">
+                    <Image src="/facebook.png" alt="" width={24} height={24} />
+                </Link>
+                <Link href="/">
+                    <Image src="/pinterest.png" alt="" width={24} height={24} />
+                </Link>
+                <Link href="/">
+                    <Image src="/linkedin.png" alt="" width={24} height={24} />
                 </Link>
             </div>
-            {/*Responsive menu here */}
+            {/* RESPONSIVE MENU */}
             <div className="md:hidden">
-                {/* Menu button */}
+                {/* MENU BUTTON */}
                 <button
                     className="w-10 h-8 flex flex-col justify-between z-50 relative"
-                    onClick={() => setIsOpen(!isOpen)}
+                    onClick={() => setOpen((prev) => !prev)}
                 >
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <motion.div
+                        variants={topVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left"
+                    ></motion.div>
+                    <motion.div
+                        variants={centerVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded"
+                    ></motion.div>
+                    <motion.div
+                        variants={bottomVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left"
+                    ></motion.div>
                 </button>
-                {/* Menu items */}
-                {isOpen && (
-                    <div className="absolute z-40 top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl font-semibold">
+                {/* MENU LIST */}
+                {open && (
+                    <motion.div
+                        variants={listVariants}
+                        initial="closed"
+                        animate="opened"
+                        className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40"
+                    >
                         {links.map((link) => (
-                            <Link
-                                key={link.url}
-                                href={link.url}
-                                className="p-2 hover:text-indigo-600"
+                            <motion.div
+                                variants={listItemVariants}
+                                className={`p-2 transition hover:bg-indigo-600 ${
+                                    pathName === link.url &&
+                                    "bg-indigo-600 text-white"
+                                }`}
+                                key={link.title}
                             >
-                                {link.title}
-                            </Link>
+                                <Link href={link.url}>{link.title}</Link>
+                            </motion.div>
                         ))}
-                    </div>
+                    </motion.div>
                 )}
             </div>
         </div>
